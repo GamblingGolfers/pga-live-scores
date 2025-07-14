c2fbcb470f46c9f483844d331c3f6b49b71f5982
GamblingGolfers <deano139@gmail.com>
2025-07-14 12:20:43 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,32 @@
+// This file replaces config.json and picks.json
+// It provides all the necessary configuration directly to the application.
+
+export const tournamentInfo = {
+  orgId: "1",
+  tournId: "026",
+  year: "2025"
+};
+
+export const gamblerNames = [
+  "Kris",
+  "Phil",
+  "Pet",
+  "Cal",
+  "Billy",
+  "Dean"
+];
+
+export const playerPicks = {
+  "37378": ["Kris", "Cal"],
+  "31323": ["Phil"],
+  "58619": ["Kris"],
+  "46046": ["Dean"],
+  "52666": ["Phil"],
+  "51766": ["Cal"],
+  "40250": ["Billy"],
+  "28237": ["Dean"],
+  "38991": ["Billy"],
+  "59018": ["Cal"],
+  "35506": ["Pet"],
+  "54628": ["Pet"]
+};
